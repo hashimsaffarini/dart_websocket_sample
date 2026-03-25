c8dde52d43e30e5d3ecca3968db0d996a672958b
hashimsaffarini <hashimsaffarini044@gmail.com>
2026-03-25 13:44:00 +0200

    first commit

---
+++ bin/dart_websocket_sample.dart
@@ -0,0 +1,5 @@
+import 'package:dart_websocket_sample/dart_websocket_sample.dart' as dart_websocket_sample;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_websocket_sample.calculate()}!');
+}
---
+++ test/dart_websocket_sample_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_websocket_sample/dart_websocket_sample.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
